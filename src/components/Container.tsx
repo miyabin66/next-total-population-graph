34e2c6e9be5d6b9fa00fb33ac875b55c411057e5
miyabin66 <freezemark5166@gmail.com>
2023-10-22 20:44:53 +0900

    コンポーネントを大まかに作成

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import { Graph } from './Graph';
+import { Selector } from './Selector';
+
+export const Contaier = () => {
+  return (
+    <>
+      <Selector />
+      <Graph />
+    </>
+  );
+};
