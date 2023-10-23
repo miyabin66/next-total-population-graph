f0aadc3c9a43644797710a0942619a88ee61f61c
miyabin66 <freezemark5166@gmail.com>
2023-10-24 00:08:50 +0900

    ラジオボタン用意

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,16 +1,33 @@
 'use client';
 import { Graph } from './Graph';
 import { Selector } from './Selector';
-import type { GetPrefecturesResponse } from '@/interfaces/prefectures';
+import { useCallback, useState } from 'react';
+import type { MouseEventHandler } from 'react';
+import type {
+  DisplayConditions,
+  GetPrefecturesResponse,
+} from '@/interfaces/prefectures';
 
 interface Props {
   PrefecturesData: GetPrefecturesResponse;
 }
 
 export const Contaier = ({ PrefecturesData }: Props) => {
+  const [displayCondition, setDisplayCondition] =
+    useState<DisplayConditions>('総人口');
+
+  const changeDisplayCondition: MouseEventHandler<HTMLInputElement> =
+    useCallback((e) => {
+      setDisplayCondition(e.currentTarget.value as DisplayConditions);
+    }, []);
+
   return (
     <>
-      <Selector PrefecturesData={PrefecturesData} />
+      <Selector
+        PrefecturesData={PrefecturesData}
+        displayCondition={displayCondition}
+        changeDisplayCondition={changeDisplayCondition}
+      />
       <Graph />
     </>
   );
--- src/interfaces/prefectures.ts
+++ src/interfaces/prefectures.ts
@@ -7,3 +7,13 @@ export interface GetPrefecturesResponse {
     },
   ];
 }
+
+export const DisplayConditionsList = {
+  総人口: '総人口',
+  年少人口: '年少人口',
+  生産年齢人口: '生産年齢人口',
+  老年人口: '老年人口',
+} as const;
+
+export type DisplayConditions =
+  (typeof DisplayConditionsList)[keyof typeof DisplayConditionsList];
