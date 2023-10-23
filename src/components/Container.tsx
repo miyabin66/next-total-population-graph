104fb6f406e4eaa95fd48a65883102bbf52126f9
Furuzono Makoto <freezemark5166@gmail.com>
2023-10-24 00:55:47 +0900

    ラジオボタンで切り替え (#42)

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,17 +1,34 @@
 'use client';
 import { Graph } from './Graph';
 import { Selector } from './Selector';
-import type { GetPrefecturesResponse } from '@/interfaces/prefectures';
+import { useCallback, useState } from 'react';
+import type { MouseEventHandler } from 'react';
+import type {
+  DisplayConditions,
+  GetPrefecturesResponse,
+} from '@/interfaces/prefectures';
 
 interface Props {
   PrefecturesData: GetPrefecturesResponse;
 }
 
 export const Contaier = ({ PrefecturesData }: Props) => {
+  const [displayCondition, setDisplayCondition] =
+    useState<DisplayConditions>('総人口');
+
+  const changeDisplayCondition: MouseEventHandler<HTMLInputElement> =
+    useCallback((e) => {
+      setDisplayCondition(e.currentTarget.value as DisplayConditions);
+    }, []);
+
   return (
     <>
-      <Selector PrefecturesData={PrefecturesData} />
-      <Graph />
+      <Selector
+        PrefecturesData={PrefecturesData}
+        displayCondition={displayCondition}
+        changeDisplayCondition={changeDisplayCondition}
+      />
+      <Graph displayCondition={displayCondition} />
     </>
   );
 };
