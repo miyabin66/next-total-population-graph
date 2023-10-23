c7b327810f060134efa304e29d22bb38dd3c7206
miyabin66 <freezemark5166@gmail.com>
2023-10-24 00:50:02 +0900

    ラジオボタンで切り替え

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,20 +1,49 @@
-import type { GetPrefecturesResponse } from '@/interfaces/prefectures';
+import {
+  DisplayConditionsList,
+  type DisplayConditions,
+  type GetPrefecturesResponse,
+} from '@/interfaces/prefectures';
+import type { MouseEventHandler } from 'react';
 
 interface Props {
   PrefecturesData: GetPrefecturesResponse;
+  displayCondition: DisplayConditions;
+  changeDisplayCondition: MouseEventHandler<HTMLInputElement>;
 }
 
-export const Selector = ({ PrefecturesData }: Props) => {
+export const Selector = ({
+  PrefecturesData,
+  displayCondition,
+  changeDisplayCondition,
+}: Props) => {
   return (
     <>
-      {PrefecturesData.result.map((item) => {
-        return (
-          <label key={item.prefCode}>
-            <input type="checkbox" value={item.prefCode} />
-            {item.prefName}
-          </label>
-        );
-      })}
+      <div>
+        {PrefecturesData.result.map((item) => {
+          return (
+            <label key={item.prefCode}>
+              <input type="checkbox" value={item.prefCode} />
+              {item.prefName}
+            </label>
+          );
+        })}
+      </div>
+      <div>
+        {Object.values(DisplayConditionsList).map((value) => {
+          return (
+            <label key={value}>
+              <input
+                type="radio"
+                name="display_conditions"
+                value={value}
+                onClick={changeDisplayCondition}
+                defaultChecked={displayCondition === value}
+              />
+              {value}
+            </label>
+          );
+        })}
+      </div>
     </>
   );
 };
