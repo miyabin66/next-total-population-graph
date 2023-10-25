037c4178b096a4137fcefce2cb413247a358e584
miyabin66 <freezemark5166@gmail.com>
2023-10-25 22:53:44 +0900

    scssの変数をどこでも呼べるようにした

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    prependData: `@use '@/styles/main.scss' as *;`,
+  },
+};
 
 module.exports = nextConfig;
