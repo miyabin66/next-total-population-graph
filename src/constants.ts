21a6d31d502bf22c30a4da74aa21e26d326ac326
miyabin66 <freezemark5166@gmail.com>
2023-10-28 11:08:15 +0900

    ビルドが失敗したのでlocalhost:3000も許可する値に入れてみる

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ export const RESAS_API_URL = 'https://opendata.resas-portal.go.jp/api/v1';
 
 export const CORS_HEADERS = {
   'Access-Control-Allow-Origin':
-    'https://miyabin-total-population-graph.vercel.app',
+    'https?://(miyabin-total-population-graph\\.vercel\\.app|localhost:3000)',
   'Access-Control-Allow-Methods': 'GET',
 };
