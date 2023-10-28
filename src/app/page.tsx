fc972a338a3e39d49e492b1d8a22347fe2ab4185
Furuzono Makoto <freezemark5166@gmail.com>
2023-10-28 12:51:12 +0900

    dataの渡し方を元に戻す (#55)
    * dataの渡し方を元に戻す
    
    * dataは{}で囲ってないと動かないのかのテスト
    
    * prefecturesだけ{}を外す
    
    * ログを仕込む
    
    * localとactionsで帰ってくるレスポンスが違うのでそれを考慮する
    
    * コードを元に戻す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,27 @@ import styles from '@/styles/page.module.scss';
 import { Contaier } from '@/components/Container';
 import { BASE_URL } from '@/env';
 import { throwError } from '@/lib/throwError';
-import type { GetPrefecturesResponse } from '@/interfaces/prefectures';
+import type {
+  GetPrefecturesActionsResponse,
+  GetPrefecturesData,
+  GetPrefecturesResponse,
+} from '@/interfaces/prefectures';
 
 export default async function Home() {
   try {
     const res = await fetch(`${BASE_URL}/api/prefectures`);
     const data = (await res.json()) as GetPrefecturesResponse;
 
+    // Actions環境とローカル・サイト環境で帰ってくるデータ形式が違うので苦肉の策
+    const prefecturesData = (
+      (data as GetPrefecturesData).result !== undefined
+        ? data
+        : (data as GetPrefecturesActionsResponse).data
+    ) as GetPrefecturesData;
+
     return (
       <main className={styles.main}>
-        <Contaier PrefecturesData={data} />
+        <Contaier prefecturesData={prefecturesData} />
       </main>
     );
   } catch (e) {
