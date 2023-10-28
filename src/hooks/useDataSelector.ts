ea151fd9ace1824b3dd5445d5a3fe8f82f5e7f7d
miyabin66 <freezemark5166@gmail.com>
2023-10-28 15:59:01 +0900

    せっかくオブジェクトで定義しているのでその値を使用する

--- src/hooks/useDataSelector.ts
+++ src/hooks/useDataSelector.ts
@@ -1,15 +1,17 @@
-import { useState, type MouseEventHandler, useCallback } from 'react';
-import type {
-  PrefecturesList,
-  DisplayConditions,
+import {
+  DisplayConditionsList,
+  type PrefecturesList,
+  type DisplayConditions,
 } from '@/interfaces/prefectures';
+import { useState, useCallback, type MouseEventHandler } from 'react';
 import type { PopulationGraphData } from '@/interfaces/population';
 
 export const useDataSelector = () => {
   const [currentPrefectures, setCurrentPrefectures] =
     useState<PrefecturesList>();
-  const [displayCondition, setDisplayCondition] =
-    useState<DisplayConditions>('総人口');
+  const [displayCondition, setDisplayCondition] = useState<DisplayConditions>(
+    DisplayConditionsList['総人口'],
+  );
   const [graphData, setGraphData] = useState<PopulationGraphData[]>();
 
   const changePrefectures: MouseEventHandler<HTMLInputElement> = useCallback(
