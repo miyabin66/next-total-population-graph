0b7f93244e99887a60d8951648714be7c73ad918
miyabin66 <freezemark5166@gmail.com>
2023-10-29 16:19:19 +0900

    undefinedの処理忘れを修正

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ import { BASE_URL } from './env';
 export const RESAS_API_URL = 'https://opendata.resas-portal.go.jp/api/v1';
 
 export const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': BASE_URL,
+  'Access-Control-Allow-Origin': BASE_URL || '',
   'Access-Control-Allow-Methods': 'GET',
 };
