8451cdbb86a12693edbcd57dd6e0edc52ff52392
miyabin66 <freezemark5166@gmail.com>
2023-11-03 17:10:20 +0900

    テストで人口データ値を確認するのではなくグラフのタイトルを確認するように変更

--- src/test/useHighcharts.test.ts
+++ src/test/useHighcharts.test.ts
@@ -1,6 +1,5 @@
 import { useHighcharts } from '@/hooks/useHighcharts';
 import { DisplayConditionsList } from '@/interfaces/prefectures';
-import { OPTIONS_SERIES } from '@/mock/highChartsData';
 import { POPULATION_GRAPH_DATA } from '@/mock/populationData';
 import { cleanup, renderHook } from '@testing-library/react';
 
@@ -17,6 +16,24 @@ describe('useHighchartsのテスト', () => {
       }),
     );
 
-    expect(result.current.options.series).toStrictEqual(OPTIONS_SERIES);
+    const options = result.current.options;
+
+    if (options.series) {
+      expect(options.series[0].name).toBe('北海道');
+    }
+    expect(options.title.text).toBe('総人口グラフ');
+  });
+
+  test('都道府県を北海道、条件に総人口を選択した後で条件を年少人口に変更する', () => {
+    const { result } = renderHook(() =>
+      useHighcharts({
+        displayCondition: DisplayConditionsList['年少人口'],
+        graphData: POPULATION_GRAPH_DATA,
+      }),
+    );
+
+    const options = result.current.options;
+
+    expect(options.title.text).toBe('年少人口グラフ');
   });
 });
