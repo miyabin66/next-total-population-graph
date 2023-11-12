a67e9109dade79d07d8db1cf19101feb78cbc01e
miyabin66 <freezemark5166@gmail.com>
2023-11-12 09:57:22 +0900

    コメントを消す

--- fixJsDomEnvironment.ts
+++ fixJsDomEnvironment.ts
@@ -4,7 +4,6 @@ export default class FixJSDOMEnvironment extends JSDOMEnvironment {
   constructor(...args: ConstructorParameters<typeof JSDOMEnvironment>) {
     super(...args);
 
-    // FIXME https://github.com/jsdom/jsdom/issues/1724
     if (!this.global.fetch) {
       this.global.fetch = fetch;
       this.global.Headers = Headers;
